995446ea71d36616e57bba6832eaebd1506df59f
Rafael D Rissmann <rafariss@gmail.com>
2022-09-23 13:41:02 -0300

    Criad metodo o lista de teste de recupeção

--- Program.cs
+++ Program.cs
@@ -1,9 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//metodo de envio de parametro
+app.MapPost("/product", (Product product) => { ProductRepository.Add(product); });
+
+//Metodo de recebimento de dados
+app.MapGet("/product/{code}", ([FromRoute]string code) => {
+    var product =  ProductRepository.GetBy(code);
+    return product;
+});
 
 app.Run();
+
+public static class ProductRepository{
+
+    public static List<Product> Products{get; set; }
+
+    public static void Add(Product product){
+        if(Products == null)
+            Products = new List<Product>();
+
+            Products.Add(product);      
+        
+        }
+         
+    
+    public static Product GetBy(string code){
+        return Products.First(p => p.Code == code);
+    }
+
+}
 
 public class Product {
     public int id {get; set; }
