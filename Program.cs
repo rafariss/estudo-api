c4920526484bc3051df64d37f42501013012599f
Rafael D Rissmann <rafariss@gmail.com>
2022-09-23 15:37:02 -0300

    Criado metodos de alteração, delete via API

--- Program.cs
+++ Program.cs
@@ -13,6 +13,19 @@ app.MapGet("/product/{code}", ([FromRoute]string code) => {
     return product;
 });
 
+//Metodo de alteração do produto
+app.MapPut("/product", (Product product) => {
+    var productSaved =  ProductRepository.GetBy(product.Code);
+    productSaved.Name =  product.Name;
+});
+
+app.MapDelete("/product/{code}", ([FromRoute]string code) =>{
+
+    var productSaved =  ProductRepository.GetBy(code);
+    ProductRepository.Remove(productSaved);
+
+});
+
 app.Run();
 
 public static class ProductRepository{
@@ -29,8 +42,16 @@ public static class ProductRepository{
          
     
     public static Product GetBy(string code){
-        return Products.First(p => p.Code == code);
+        return Products.FirstOrDefault(p => p.Code == code);
+    
     }
+
+     //metodo de remoção
+    public static void Remove(Product product){
+
+        Products.Remove(product);
+    }
+    
 
 }
 
